258e3b6d73895a2de0bc306c77a6079b70ecc2aa
eirikhanasand <eirik.hanasand@gmail.com>
2026-03-27 18:46:16 +0100

    Removed env file comments and updated deps

--- lb/api/src/constants.ts
+++ lb/api/src/constants.ts
@@ -1,30 +1,4 @@
-// import dotenv from 'dotenv'
-
-// dotenv.config({ path: '../.env' })
-
-// const requiredEnvironmentVariables: string[] = [
-    
-// ]
-
-// const missingVariables = requiredEnvironmentVariables.filter(
-//     (key) => !process.env[key]
-// )
-
-// if (missingVariables.length > 0) {
-//     throw new Error(
-//         'Missing essential environment variables:\n' +
-//         missingVariables
-//             .map((key) => `${key}: ${process.env[key] || 'undefined'}`)
-//             .join('\n')
-//     )
-// }
-
-// const env = Object.fromEntries(
-//     requiredEnvironmentVariables.map((key) => [key, process.env[key]])
-// )
-
 const config = {
-    // PORT: env.API_PORT,
     PORT: 8080,
     CACHE_TTL: 30000
 }
