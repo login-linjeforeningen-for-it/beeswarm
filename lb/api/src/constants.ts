08e19d689ddf3313df9d19525a8fb9d8304d1abc
eirikhanasand <eirik.hanasand@gmail.com>
2026-03-27 18:40:39 +0100

    Removed db leftovers

--- lb/api/src/constants.ts
+++ lb/api/src/constants.ts
@@ -1,30 +1,31 @@
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 
-dotenv.config({ path: '../.env' })
+// dotenv.config({ path: '../.env' })
 
-const requiredEnvironmentVariables: string[] = [
+// const requiredEnvironmentVariables: string[] = [
     
-]
+// ]
 
-const missingVariables = requiredEnvironmentVariables.filter(
-    (key) => !process.env[key]
-)
+// const missingVariables = requiredEnvironmentVariables.filter(
+//     (key) => !process.env[key]
+// )
 
-if (missingVariables.length > 0) {
-    throw new Error(
-        'Missing essential environment variables:\n' +
-        missingVariables
-            .map((key) => `${key}: ${process.env[key] || 'undefined'}`)
-            .join('\n')
-    )
-}
+// if (missingVariables.length > 0) {
+//     throw new Error(
+//         'Missing essential environment variables:\n' +
+//         missingVariables
+//             .map((key) => `${key}: ${process.env[key] || 'undefined'}`)
+//             .join('\n')
+//     )
+// }
 
-const env = Object.fromEntries(
-    requiredEnvironmentVariables.map((key) => [key, process.env[key]])
-)
+// const env = Object.fromEntries(
+//     requiredEnvironmentVariables.map((key) => [key, process.env[key]])
+// )
 
 const config = {
-    PORT: env.API_PORT,
+    // PORT: env.API_PORT,
+    PORT: 8080,
     CACHE_TTL: 30000
 }
 
