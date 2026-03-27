4823b3b3e2a4e423b14e962ec680223a7ca9e848
eirikhanasand <eirik.hanasand@gmail.com>
2026-03-27 18:58:14 +0100

    Switched from dev to prod urls

--- lb/ui/src/config.ts
+++ lb/ui/src/config.ts
@@ -2,10 +2,10 @@ import packagejson from '../package.json'
 
 const config = {
     url: {
-        api: 'http://localhost:8080/api',
-        // api: 'https://api.beeswarm.login.no/api',
-        api_ws: 'ws://localhost:8080/api',
-        // api_ws: 'wss://api.beeswarm.login.no/api',
+        // api: 'http://localhost:8080/api',
+        api: 'https://beeswarmed.login.no/api',
+        // api_ws: 'ws://localhost:8080/api',
+        api_ws: 'wss://beeswarmed.login.no/api',
     },
     version: packagejson.version,
     light: 'shadow-[inset_0_1px_0_rgba(255,255,255,0.2),0_4px_8px_rgba(0,0,0,0.4)] backdrop-blur-md'
